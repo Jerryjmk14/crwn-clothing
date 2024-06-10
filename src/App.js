cedc692f87584e450f89c4ea3d418577d2476d47
Jeremiah Egwa <jerriee232@gmail.com>
2024-06-10 12:34:24 +0100

    Created Navigation component and Sign In component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
 import { Routes, Route } from "react-router-dom";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 function App() {
-  const Shoppage = () => <h1>I am Hungrt</h1>;
+  const Shoppage = () => <h1>I am the SHOP page</h1>;
+
   return (
     <Routes>
-      <Route path="/home" element={<Home />}>
-        <Route path="/home/shop" element={<Shoppage />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shoppage />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
