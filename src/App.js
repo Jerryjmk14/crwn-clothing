186124ee20c13134f714ef7077031c1fed08edc8
Jeremiah Egwa <jerriee232@gmail.com>
2024-06-22 18:42:29 +0100

    modified signup component and created signin componet and authentication component with respective functionalities

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import { Routes, Route } from "react-router-dom";
-import SignIn from "./routes/sign-in/sign-in.component";
+import Authentication from "./routes/authentication/authentication.component";
 
 function App() {
   const Shoppage = () => <h1>I am the SHOP page</h1>;
@@ -11,7 +11,7 @@ function App() {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shoppage />} />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
